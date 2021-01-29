104ddac23a0bcd58bcc0f947ea2d2d1e32a5eeed
DESKTOP-SIR5HGD\maxky <teerapat.chit@bumail.net>
2021-01-29 20:39:09 +0700

    Send Assignment Week1

---
+++ GI455_UnityProject/Assets/word.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class word : MonoBehaviour
+{
+    public GameObject inputField;
+    public GameObject textDisplay;
+    string inPutkey;
+
+    public void StoreName()
+    {
+
+        inPutkey = inputField.GetComponent<Text>().text;
+        {
+            if (inPutkey == "Red")
+            {
+                textDisplay.GetComponent<Text>().text = "[" + inPutkey + "]" + " is found ";
+            }
+            else if (inPutkey == "Blue")
+            {
+                textDisplay.GetComponent<Text>().text = "[" + inPutkey + "]" + " is found ";
+            }
+            else if (inPutkey == "Green")
+            {
+                textDisplay.GetComponent<Text>().text = "[" + inPutkey + "]" + " is found ";
+            }
+            else if (inPutkey == "Yellow")
+            {
+                textDisplay.GetComponent<Text>().text = "[" + inPutkey + "]" + " is found ";
+            }
+            else if (inPutkey == "Black")
+            {
+                textDisplay.GetComponent<Text>().text = "[" + inPutkey + "]" + " is found ";
+            }
+            else
+            {
+                textDisplay.GetComponent<Text>().text = "[" + inPutkey + "]" + " is not found ";
+            }
+
+
+
+        }
+    }
+}
